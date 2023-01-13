b0121d0c808f96ec88ffcee46b126530a96bbe3a
Guillemet Samuel <samuel.guillemet@telecom-sudparis.eu>
2023-01-13 10:48:41 +0100

    Update HeroController for dash

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -5,12 +5,22 @@ using UnityEngine.AI;
 
 public class HeroController : MonoBehaviour
 {
-    [SerializeField]
-    Camera _cam;
+    private enum AgentRangeMode
+    {
+        Walk,
+        Dash
+    }
+
+    [SerializeField] Camera _cam;
     private float _horizontalInput;
     private float _verticalInput;
 
+    private AgentRangeMode _agentRange = AgentRangeMode.Walk;
+    // Return -1f for AgentRangeMode.Walk and -0.1f pour AgentRangeMode.Dash
+    private float AgentRange => _agentRange == AgentRangeMode.Walk ? -1f : -0.1f;
+
     NavMeshAgent _agent;
+
     // Animator _animator;
 
     // Start is called before the first frame update
@@ -31,42 +41,19 @@ public class HeroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
-
-        // Calculate the direction of the movement based on input and rotation
-        Vector3 direction = new Vector3(_horizontalInput, -1f, _verticalInput);
-
-        // Compute a raycast from the player with the direction to get the y position of the ground to set the right y postion for the agent
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, direction, out hit))
-        {
-            direction.y = hit.point.y;
-            Debug.DrawLine(transform.position, hit.point, Color.red);
-        }
-
-        if (direction.magnitude > 0.1f)
-        {
-            Vector3 destination = new Vector3(transform.position.x + direction.x, direction.y, transform.position.z + direction.z);
-
-            _agent.SetDestination(destination);
-            UpdateRotation();
-        }
-
-
-        // Quaternion rotation = transform.rotation;
-
-        // Vector3 input = new Vector3(_horizontalInput, 0, _verticalInput);
-        // Vector3 animspeed = new Vector3(Vector3.Dot(input, transform.forward), 0, Vector3.Dot(input, transform.right));
-
-        // float horizontalSpeed = -animspeed.x;
-        // float verticalSpeed = -animspeed.z;
-
-        // _animator.SetFloat("HorizontalSpeed", horizontalSpeed * (_agent.velocity.magnitude / _agent.speed));
-        // _animator.SetFloat("VerticalSpeed", verticalSpeed * (_agent.velocity.magnitude / _agent.speed));
+        CalculateDestinationWithInput();
+        UpdateRotation();
 
         _cam.transform.position = new Vector3(x: transform.position.x, y: 33, z: transform.position.z - 10);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _agentRange = AgentRangeMode.Dash;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _agentRange = AgentRangeMode.Walk;
+        }
 
     }
 
@@ -80,5 +67,57 @@ public class HeroController : MonoBehaviour
         {
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
         }
+    }
+
+    private void CalculateDestinationWithClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void CalculateDestinationWithInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the direction of the movement based on input and rotation
+        Vector3 direction = new Vector3(_horizontalInput, AgentRange, _verticalInput);
+
+        Debug.DrawRay(transform.position, direction, Color.green);
+
+
+        // Compute a raycast from the player with the direction to get the y position of the ground to set the right y postion for the agent
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            Vector3 destination = new Vector3(transform.position.x + _horizontalInput, hit.point.y, transform.position.z + _verticalInput);
+
+            if (_agentRange == AgentRangeMode.Dash) destination = hit.point;
+
+            _agent.SetDestination(destination);
+
+            Debug.DrawLine(transform.position, destination, Color.blue);
+        }
+
+
+        // Quaternion rotation = transform.rotation;
+
+        // Vector3 input = new Vector3(_horizontalInput, 0, _verticalInput);
+        // Vector3 animspeed = new Vector3(Vector3.Dot(input, transform.forward), 0, Vector3.Dot(input, transform.right));
+
+        // float horizontalSpeed = -animspeed.x;
+        // float verticalSpeed = -animspeed.z;
+
+        // _animator.SetFloat("HorizontalSpeed", horizontalSpeed * (_agent.velocity.magnitude / _agent.speed));
+        // _animator.SetFloat("VerticalSpeed", verticalSpeed * (_agent.velocity.magnitude / _agent.speed));
     }
 }
