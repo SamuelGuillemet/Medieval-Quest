6cf74d74d1da168d9591b7bb4782908806eef133
Thibaut Sauv <sauvthibaut@gmail.com>
2023-01-14 16:17:20 +0100

    Added pause menu interactions from in game UI

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button resumeButton;
+    public Button exitButton;
+    public Button settingsButton;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        //save current scene
+        PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("SettingsMenu");
+    }
+}
