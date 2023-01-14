69526292d9ace8b8d5a0b8095438deba512a02b6
Thibaut Sauv <sauvthibaut@gmail.com>
2023-01-14 16:16:34 +0100

    Added timer to in game UI

---
+++ Assets/Scripts/UITimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{
+    public TMPro.TMP_Text TimerText;
+    public bool playing { get; set; }
+    private float timer;
+
+    private void Awake()
+    {
+        playing = true;
+        timer = 0;
+    }
+
+    void Update()
+    {
+        if (playing == true)
+        {
+            timer += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timer / 60F);
+            int seconds = Mathf.FloorToInt(timer % 60F);
+            int milliseconds = Mathf.FloorToInt((timer * 100F) % 100F);
+            TimerText.SetText(
+                minutes.ToString("00")
+                    + ":"
+                    + seconds.ToString("00")
+                    + ":"
+                    + milliseconds.ToString("00")
+            );
+        }
+    }
+}
