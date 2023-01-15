561f4ab87d5b183dcf1df73f06fcc224b9360000
Thibaut Sauv <sauvthibaut@gmail.com>
2023-01-16 00:48:10 +0100

    Update GameUI to change HP bar depending on selected character

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -2,5 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameUI : MonoBehaviour { }
+public class GameUI : MonoBehaviour
+{
+    private Slider _healthBar;
+    private Slider _xpBar;
+
+    // private Slider _ability1;
+    // private Slider _ability2;
+    // private Slider _ability3;
+
+    private void Start()
+    {
+        _healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+        _xpBar = GameObject.Find("XPBar").GetComponent<Slider>();
+
+        _healthBar.maxValue = PlayerPrefs.GetFloat("Health");
+        _healthBar.value = _healthBar.maxValue;
+
+        _xpBar.value = _xpBar.minValue;
+
+        // _ability1.value = _ability1.maxValue;
+        // _ability2.value = _ability2.maxValue;
+        // _ability3.value = _ability3.maxValue;
+    }
+}
