60d43fd754216ea6032db9ecf607fc3439c8051d
Thibaut Sauv <sauvthibaut@gmail.com>
2023-01-18 13:22:09 +0100

    Started implementing GameManager to Upgrades

--- Assets/Scripts/UpgradeMenu.cs
+++ Assets/Scripts/UpgradeMenu.cs
@@ -6,23 +6,36 @@ using UnityEngine.UI;
 public class UpgradeMenu : MonoBehaviour
 {
     public GameObject upgradeMenu;
+    private GameObject specificUpgrades;
+    private GameManager _gameManager;
     Button[] upgradeButtons;
     Slider[] upgradeSliders;
 
     void Awake()
     {
-        upgradeButtons = upgradeMenu.GetComponentsInChildren<Button>();
+        _gameManager = GameObject.Find("GameManager").GetComponentInParent<GameManager>();
+
         upgradeMenu.SetActive(false);
         upgradeSliders = upgradeMenu.GetComponentsInChildren<Slider>();
-        foreach (Slider slider in upgradeSliders)
+        for (int i = 0; i < upgradeSliders.Length; i++)
         {
-            slider.value = 0;
+            upgradeSliders[i].value = 0;
+            upgradeButtons[i] = upgradeSliders[i].GetComponentInChildren<Button>();
         }
+
+        specificUpgrades = GameObject.Find("SpecificUpgrades");
     }
 
     public void Upgrade(Slider slider)
     {
+        int upgradeIndex = System.Array.IndexOf(upgradeSliders, slider);
+        //StartCoroutine(Upgrade(_gameManager.SelectedPlayer, upgradeIndex));
         slider.value += 1;
+        if (slider.value == slider.maxValue)
+        {
+            slider.GetComponentInChildren<Button>().interactable = false;
+        }
+
         StartCoroutine(ExitMenu());
     }
 
