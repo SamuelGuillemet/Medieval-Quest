2d9752a98d44709a097e997007c104c9b79aab1d
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:29:49 +0100

    Rename archer attack script

--- Assets/Scripts/ArcherAttack.cs
+++ Assets/Scripts/ArcherAttack.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttacks : MonoBehaviour
+public class ArcherAttack : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
