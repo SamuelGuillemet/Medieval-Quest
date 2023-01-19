a235d21ce170d7b45d7a3f83bb62aaa29698d2aa
Guillemet Samuel <samuel.guillemet@telecom-sudparis.eu>
2023-01-19 04:10:25 +0100

    Create Editor Script to enable Enemy Attack animation

---
+++ Assets/Editor/WarriorControllerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using WarriorAnimsFREE;
+
+[CustomEditor(typeof(WarriorController))]
+public class WarriorControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WarriorController warriorController = (WarriorController)target;
+
+        DrawDefaultInspector();
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Attack"))
+        {
+            warriorController.Attack1();
+        }
+    }
+}
