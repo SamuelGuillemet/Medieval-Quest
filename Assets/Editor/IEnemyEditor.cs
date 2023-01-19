c54cde2273275de643b73a61ef174e6f0d0efb90
Guillemet Samuel <samuel.guillemet@telecom-sudparis.eu>
2023-01-19 04:10:44 +0100

    Add Editor Script to disable an enable an enemy

---
+++ Assets/Editor/IEnemyEditor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(IEnemy), true)]
+public class IEnemyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        IEnemy iEnemy = (IEnemy)target;
+
+        DrawDefaultInspector();
+
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(iEnemy.transform.position);
+        if (screenPoint.x < 0f || screenPoint.x > 1f || screenPoint.y < 0f || screenPoint.y > 1f)
+        {
+            GUI.color = Color.red;
+        }
+        else
+        {
+            GUI.color = Color.green;
+        }
+        EditorGUILayout.Vector2Field("Screen Point", new Vector2(screenPoint.x, screenPoint.y));
+        GUI.color = Color.white;
+
+
+        EditorGUILayout.Space(10);
+
+        if (GUILayout.Button("DeactivateEnemy"))
+        {
+            iEnemy.DeactivateEnemy();
+        }
+
+        if (GUILayout.Button("ActivateEnemy"))
+        {
+            iEnemy.ActivateEnemy();
+        }
+    }
+}
