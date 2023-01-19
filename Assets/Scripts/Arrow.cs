46ff260b2fb87ad0b58688b61985eb0c00ab3f93
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:33:54 +0100

    Add script for Arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public float speed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
