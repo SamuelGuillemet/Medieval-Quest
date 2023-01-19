61d1b1629451f698aa072654b52f3417e7abc93e
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:40:49 +0100

    Update collision for Arrow.cs

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -6,16 +6,36 @@ public class Arrow : MonoBehaviour
 {
 
     public float speed = 40f;
+    public float damage;
+    public int enemyTouched = 0;
+    public int maxEnemyTouched = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.position -= transform.right * speed * Time.deltaTime;
     }
+
+    void OnCollisionEnter(Collision infoCollision) 
+    {
+        if (infoCollision.gameObject.name == "Enemy")
+        {
+            enemyTouched += 1;
+            //makeDamageToEnemy
+        }
+        Debug.Log(infoCollision.gameObject);
+        if ((infoCollision.gameObject.tag != "Player" && infoCollision.gameObject.name != "Enemy") || enemyTouched == maxEnemyTouched )
+        {
+            Destroy(gameObject);
+        }
+
+    } 
+
 }
