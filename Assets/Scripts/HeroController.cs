9b48778c8221ae8257b1d9680214308a72285aa6
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:41:00 +0100

    Remove isMoving

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -28,8 +28,6 @@ public class HeroController : MonoBehaviour
 
     Animator _animator;
 
-    public bool isMoving;
-
 
     // Start is called before the first frame update
     void Start()
@@ -120,14 +118,6 @@ public class HeroController : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
-
-        if (_horizontalInput == 0 && _verticalInput == 0)
-        {
-            isMoving = false; 
-        } else
-        {
-            isMoving = true;
-        }
 
 
         // Calculate the direction of the movement based on input and rotation
