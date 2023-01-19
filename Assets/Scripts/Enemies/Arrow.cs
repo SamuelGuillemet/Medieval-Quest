f66922da834fdb2e036203769d294d23ee959189
Guillemet Samuel <samuel.guillemet@telecom-sudparis.eu>
2023-01-19 04:08:51 +0100

    Create Arrow Prefab

---
+++ Assets/Scripts/Enemies/Arrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private int _damage = 3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player hit");
+            DisableObject();
+        }
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            DisableObject();
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < -10) DisableObject();
+        if (transform.position.y > 10) DisableObject();
+    }
+
+    private void DisableObject()
+    {
+        Destroy(gameObject); //TODO: Pooling
+    }
+}
