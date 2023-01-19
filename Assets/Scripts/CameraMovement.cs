af94d0fe6aaeedb34bcbf3ae62291e21a5a0882d
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:20:06 +0100

    2 Cameras tests + player movement + texture of the first character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 v3 = new Vector3(0,0,0);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            v3 += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            v3 += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            v3 += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            v3 -= Vector3.forward;
+        }
+
+        transform.Translate(v3 * Time.deltaTime * speed);
+    }
+}
