deab5458f35b845562cb3313f96e45c48e47aabd
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-19 11:38:54 +0100

    Update TrapDoor script

--- Assets/AurynSky/Dungeon Pack/Scripts/TrapDoorDemo.cs
+++ Assets/AurynSky/Dungeon Pack/Scripts/TrapDoorDemo.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class TrapDoorDemo : MonoBehaviour {
 
-    //This script goes on the TrapDoor prefab;
 
-    public Animator TrapDoorAnim; //Animator for the trap door;
+    public Animator TrapDoorAnim; 
 
     // Use this for initialization
     void Awake()
     {
-        //get the Animator component from the trap;
         TrapDoorAnim = GetComponent<Animator>();
-        //start opening and closing the trap for demo purposes;
-        StartCoroutine(OpenCloseTrap());
     }
 
 
-    IEnumerator OpenCloseTrap()
+    private void OnTriggerEnter(Collider infoCollision)
     {
-        //play open animation;
-        TrapDoorAnim.SetTrigger("open");
-        //wait 2 seconds;
-        yield return new WaitForSeconds(2);
-        //play close animation;
-        TrapDoorAnim.SetTrigger("close");
-        //wait 2 seconds;
-        yield return new WaitForSeconds(2);
-        //Do it again;
-        StartCoroutine(OpenCloseTrap());
-
+        if (infoCollision.gameObject.tag == "Enemy")
+        {
+            //Immobilise le'enemy oendant trois secondes 
+            Destroy(gameObject);
+        }
     }
+
 }
