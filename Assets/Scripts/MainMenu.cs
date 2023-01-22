187921401d18caea61ec05913173cae7de4aab76
Thibaut Sauv <sauvthibaut@gmail.com>
2023-01-22 01:10:53 +0100

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
-        StartCoroutine(crossFade.LoadSceneCoroutine("CharacterSelect"));
+        StartCoroutine(crossFade.LoadSceneCoroutine("GameScene"));
     }
 
     public void SettingsMenu()
