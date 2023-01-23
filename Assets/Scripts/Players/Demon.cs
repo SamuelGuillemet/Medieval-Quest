7ab11da65359918da2686ece7d98d389bca0cf54
TheoTSP <lardeurgerszteintheo@gmail.com>
2023-01-24 00:03:10 +0100

    Demon : ChocWave Attack + Sounds

--- Assets/Scripts/Players/Demon.cs
+++ Assets/Scripts/Players/Demon.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Demon : IPlayer
 {
-    [SerializeField] float damage = 2f;
-    [SerializeField] float attackRadius = 2f;
+    [SerializeField] private float damage = 2f;
+    [SerializeField] private float attackRadius = 2f;
+
+    [SerializeField] public float magnitude = 0.001f;
+    [SerializeField] public float chocWaveRadius = 3f;
+    [SerializeField] public float chocWaveDamage = 3f;
+    [SerializeField] public Camera cam;
+
+
 
 
     // Start is called before the first frame update
@@ -24,6 +31,7 @@ public class Demon : IPlayer
     {
         _canAction1 = false;
         _animator.SetTrigger("Attack");
+        _audioManager.PlaySound("AttackDemon");
         yield return new WaitForSeconds(_cooldown1);
         _canAction1 = true;
     }
@@ -44,16 +52,40 @@ public class Demon : IPlayer
     public override IEnumerator Attack2()
     {
         _canAction2 = false;
-        _animator.SetTrigger("Invicible");
+        _animator.SetTrigger("ChocWave");
+        _audioManager.PlaySound("DemonChocWave");
         yield return new WaitForSeconds(_cooldown2);
         _canAction2 = true;
     }
 
-    public void SwitchInvincible()
+    public void ChocWave()
     {
-        //Make the hero invicible 
+        StartCoroutine(Shake());
+        Collider[] cols = Physics.OverlapSphere(transform.position, chocWaveRadius);
+        foreach (Collider col in cols)
+        {
+            if (col.gameObject.tag == "Enemy")
+            {
+                //Make damage to enemy with WallDamage
+            }
+        }
     }
 
+
+    IEnumerator Shake()
+    {
+        Vector3 originalPos = cam.transform.localPosition;
+        float elapsed = 0.0f;
+        while (elapsed < 1)
+        {
+            float x = Random.Range(-0.5f, 0.5f) * magnitude;
+            float y = Random.Range(-0.5f, 0.5f) * magnitude;
+            cam.transform.localPosition = new Vector3(originalPos.x + x,originalPos.y + y, originalPos.z);
+            elapsed += Time.deltaTime * 5;
+            yield return null;
+        }
+        cam.transform.localPosition = originalPos;
+    }
 
 
 
@@ -61,6 +93,7 @@ public class Demon : IPlayer
     {
         _canAction3 = false;
         _animator.SetTrigger("Summon");
+        _audioManager.PlaySound("DemonSummon");
         yield return new WaitForSeconds(_cooldown3);
         _canAction3 = true;
     }
@@ -75,6 +108,11 @@ public class Demon : IPlayer
     public void SwitchMovement()
     {
         _playerAgent.Agent.isStopped = !_playerAgent.Agent.isStopped;
+    }
+
+    public override void DamageSound()
+    {
+        _audioManager.PlaySound("DamageDemon");
     }
 }
 
