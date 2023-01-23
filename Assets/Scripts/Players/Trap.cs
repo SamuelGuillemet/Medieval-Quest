dac5badf19ba7a5d6924dd0455b995dff1302557
Guillemet Samuel <samu.guillemet0@gmail.com>
2023-01-23 23:45:19 +0100

    Recreate Trap Door

---
+++ Assets/Scripts/Players/Trap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    private int _damage;
+    private int _maxEnemyTouched;
+    private int _enemyTouched;
+    private float _time;
+    public Animator TrapDoorAnim;
+
+    public int Damage { set => _damage = value; }
+    public int MaxEnemyTouched { set => _maxEnemyTouched = value; }
+
+    // Use this for initialization
+    void Awake()
+    {
+        _enemyTouched = 0;
+        TrapDoorAnim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (_enemyTouched == _maxEnemyTouched)
+        {
+            TrapDoorAnim.SetBool("open", true);
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            _enemyTouched += 1;
+            IEnemy _enemy = other.gameObject.GetComponent<IEnemy>();
+            Debug.Log("Enemy touched");
+            GameManager.Instance.OnEnemyDamageTaken?.Invoke(_damage, _enemy, 0);
+            _enemy.FreezeEnemy(_time);
+        }
+    }
+
+}
